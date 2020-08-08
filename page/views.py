09d7bca58747c5112ca469ac840e81019a9108e2
jinahkwak <wlsdk8949@naver.com>
2020-08-08 20:30:06 +0900

    home design

--- page/views.py
+++ page/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def result(request):
+    return render(request,'result.html')    
